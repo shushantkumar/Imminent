10cc4326292fdceff4e843272f4bdf3d13bb3e75
16CO218 <edwinthomas444@gmail.com>
2018-10-05 17:48:33 +0400

    added something

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
+import { CookieService } from 'ngx-cookie-service';
+
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
@@ -7,20 +9,61 @@ import { MatDialog, MatDialogRef } from '@angular/material';
 })
 export class ReportComponent implements OnInit {
 
+  reportDetails={
+    doctorID:"",
+    diagnosis:"",
+    comment:"",
+    medicine:[],
+    //{
+    //   'medicineName':"",
+    //   'duration':"",
+    //   'dosage':"",
+    // },
+    studentID:"",
+    bp:"",
+    bmi:"",
+    pulse:"",
+    weight:""
+  };
+
+  varName="";
+  varTokenNumber="";
+  varDiagnosis="";
+  varPrescription="";
+  varBp="";
+  varWeight="";
+  varPulse="";
+  varBmi="";
+
   private fieldArray: Array<any> = [];
   private newAttribute: any = {};
-  constructor(public dialogRef: MatDialogRef<ReportComponent>) { }
+  constructor(public dialogRef: MatDialogRef<ReportComponent>,private cookieService:CookieService) { 
+
+  }
   
 
-  addFieldValue() {
+  addFieldValue(){
       this.fieldArray.push(this.newAttribute)
       this.newAttribute = {};
   }
-
-  deleteFieldValue(index) {
+  deleteFieldValue(index){
       this.fieldArray.splice(index, 1);
   }
   onSubmit(){
+    this.reportDetails.medicine=this.fieldArray;
+
+    this.reportDetails.bp=this.varBp;
+    this.reportDetails.pulse=this.varPulse;
+    this.reportDetails.weight=this.varWeight;
+    this.reportDetails.bmi=this.varBmi;
+
+    this.reportDetails.diagnosis=this.varDiagnosis;
+    this.reportDetails.comment=this.varPrescription;
+
+    console.log(this.reportDetails);
+    //this.reportDetails.doctorID=this.cookieService.get('DOCuserID');
+
+
     this.dialogRef.close();
   }
 
